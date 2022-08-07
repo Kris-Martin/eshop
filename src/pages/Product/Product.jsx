52f8e02bbac0b117e6d99e71d36a4918da9c2683
Kris Martin <28503716+Kris-Martin@users.noreply.github.com>
2022-08-07 22:18:18 +0930

    Make add to cart and favourite buttons functional

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -2,11 +2,13 @@ import styles from "./Product.module.scss";
 import { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { ProductContext } from "../../context/ProductContext.jsx";
+import { updateCart, updateProduct } from "../../services/server.js";
 
 const Product = () => {
     const { products, setProducts } = useContext(ProductContext);
     const { productId } = useParams();
     const [product, setProduct] = useState("");
+    const [quantity, setQuantity] = useState(0);
 
     const getProduct = () => {
         setProduct(products.filter((product) => product.id === productId)[0]);
@@ -16,6 +18,31 @@ const Product = () => {
         getProduct();
     }, []);
 
+    const handleQuantityChange = (e) => {
+        const n = e.target.value;
+        setQuantity(n);
+    };
+
+    const handleAddToCart = async () => {
+        await updateCart({ productId: productId, quantity: quantity });
+        console.log(quantity, productId);
+        const productUpdate = { ...product };
+        productUpdate["stock"] = product.stock - quantity;
+        setProduct(productUpdate);
+        await updateProduct(productUpdate, productId);
+    };
+
+    const handleFavouriteClick = async (e) => {
+        const productUpdate = { ...product };
+        productUpdate["favourite"] = !product.favourite;
+        setProduct(productUpdate);
+        e.target.style.backgroundColor = productUpdate.favourite
+            ? "red"
+            : "white";
+        e.target.style.color = productUpdate.favourite ? "white" : "black";
+        await updateProduct(productUpdate, productId);
+    };
+
     const images = require.context("../../assets/images", true);
 
     if (product) {
@@ -23,11 +50,39 @@ const Product = () => {
         // console.log(product);
         return (
             <div className={styles.Product}>
-                <button>Favourite</button>
                 <img src={image} alt="" />
                 <h1>{product.name}</h1>
+                <div className={styles.Product__CartButtons}>
+                    <label htmlFor="quantity">Quantity: </label>
+                    <input
+                        type="number"
+                        name="quantity"
+                        id="quantity"
+                        onChange={handleQuantityChange}
+                    />
+                    <button
+                        className={styles.Product__Button}
+                        onClick={handleAddToCart}
+                    >
+                        Add to Cart
+                    </button>
+                    <button
+                        id="favourite"
+                        onClick={handleFavouriteClick}
+                        className={styles.Product__Button}
+                    >
+                        Favourite
+                    </button>
+                </div>
                 <h2>Brand: {product.brand}</h2>
-                <h2>Price: {product.price}</h2>
+                <h2>
+                    Price:{" "}
+                    {product.price.toLocaleString("en-AU", {
+                        style: "currency",
+                        currency: "AUD",
+                        minimumFractionDigits: 2,
+                    })}
+                </h2>
                 <h2>Qty Available: {product.stock}</h2>
                 <h2>Colours: {product.colour.join(", ")}</h2>
                 <h2>Details: </h2>
--- src/services/server.js
+++ src/services/server.js
@@ -28,3 +28,32 @@ export const getProducts = async () => {
 
     return data;
 };
+
+export const updateProduct = async (record, id) => {
+    const collectionRef = firestore.collection("products");
+
+    const docRef = collectionRef.doc(id);
+    await docRef.update(record);
+};
+
+export const getCart = async () => {
+    const collectionRef = firestore.collection("carts");
+
+    const querySnap = await collectionRef.get();
+
+    const documents = querySnap.docs;
+
+    const data = documents.map((doc) => {
+        return { id: doc.id, ...doc.data() };
+    });
+
+    return data[0];
+};
+
+export const updateCart = async (record) => {
+    const id = "uS1msJsj4jSiHpsuaM59";
+    const collectionRef = firestore.collection("carts");
+
+    const docRef = collectionRef.doc(id);
+    await docRef.update(record);
+};
