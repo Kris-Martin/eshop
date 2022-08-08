f5993a3d654965b79b20e2399a07682d5830a1c9
Kris Martin <28503716+Kris-Martin@users.noreply.github.com>
2022-08-08 11:29:17 +0930

    Create seperate CartItem component

---
+++ src/components/CartItem/CartItem.jsx
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import styles from "./CartItem.module.scss";
+import { getCart, updateCart, updateProduct } from "../../services/server.js";
+
+const CartItem = ({ cart, setCart, products, product, i }) => {
+    const [quantity, setQuantity] = useState(product.quantity);
+
+    const getProduct = (id) => {
+        const product = products.filter((product) => product.id === id)[0];
+        return product;
+    };
+
+    const deleteItem = async (e) => {
+        const updatedCart = await getCart();
+        const toDelete = Number(e.target.parentElement.id);
+        console.log(toDelete);
+
+        const productId = cart[toDelete].productId;
+        const product = getProduct(productId);
+        product.stock += Number(cart[toDelete].quantity);
+        await updateProduct(product, productId);
+
+        const updated = cart.filter((p, i) => i !== toDelete);
+        updatedCart.products = updated;
+        setCart(updated);
+        await updateCart(updatedCart);
+    };
+
+    const increaseItemCount = async (e) => {
+        const updatedCart = await getCart();
+        const toChange = Number(e.target.parentElement.id);
+        const productId = cart[toChange].productId;
+        const product = getProduct(productId);
+        const currentQuantity = Number(cart[toChange].quantity) + 1;
+
+        if (currentQuantity > product.stock)
+            return alert(
+                `Only ${product.stock} available, please reduce the size of your order.`,
+            );
+
+        product.stock -= 1;
+        await updateProduct(product, productId);
+
+        updatedCart.products[toChange].quantity = currentQuantity;
+        setQuantity(currentQuantity);
+        setCart(updateCart.products);
+        await updateCart(updatedCart);
+    };
+
+    const decreaseItemCount = async (e) => {
+        const updatedCart = await getCart();
+        const toChange = Number(e.target.parentElement.id);
+        const productId = cart[toChange].productId;
+        const currentQuantity = Number(cart[toChange].quantity) - 1;
+        if (currentQuantity === 1) e.target.disabled = true;
+
+        const product = getProduct(productId);
+        product.stock += 1;
+        await updateProduct(product, productId);
+
+        updatedCart.products[toChange].quantity = currentQuantity;
+        setQuantity(currentQuantity);
+        setCart(updateCart.products);
+        await updateCart(updatedCart);
+    };
+
+    return (
+        <div key={i} className={styles.CartItem} id={i}>
+            <h2>{product.name}</h2>
+            <p>{product.colour}</p>
+            <button
+                className={styles.CartItem__Button}
+                onClick={decreaseItemCount}
+            >
+                -
+            </button>
+            <p>{quantity}</p>
+            <button
+                className={styles.CartItem__Button}
+                onClick={increaseItemCount}
+            >
+                +
+            </button>
+            <p>
+                item:{" "}
+                {product.itemPrice.toLocaleString("en-AU", {
+                    style: "currency",
+                    currency: "AUD",
+                    minimumFractionDigits: 2,
+                })}
+            </p>
+            <p>
+                total:{" "}
+                {(product.itemPrice * product.quantity).toLocaleString(
+                    "en-AU",
+                    {
+                        style: "currency",
+                        currency: "AUD",
+                        minimumFractionDigits: 2,
+                    },
+                )}
+            </p>
+            <button className={styles.CartItem__Button} onClick={deleteItem}>
+                x
+            </button>
+        </div>
+    );
+};
+
+export default CartItem;
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,12 +1,8 @@
 import styles from "./Cart.module.scss";
 import { useEffect, useState, useContext } from "react";
-import {
-    getCart,
-    getProducts,
-    updateCart,
-    updateProduct,
-} from "../../services/server.js";
+import { getCart, getProducts } from "../../services/server.js";
 import { ProductContext } from "../../context/ProductContext.jsx";
+import CartItem from "../../components/CartItem/CartItem.jsx";
 
 const Cart = () => {
     const { products, setProducts } = useContext(ProductContext);
@@ -23,111 +19,20 @@ const Cart = () => {
         getData();
     }, []);
 
-    const getProduct = (id) => {
-        const product = products.filter((product) => product.id === id)[0];
-        return product;
-    };
-
-    const deleteItem = async (e) => {
-        const updatedCart = await getCart();
-        const toDelete = Number(e.target.parentElement.id);
-        console.log(toDelete);
-
-        const productId = cart[toDelete].productId;
-        const product = getProduct(productId);
-        product.stock += Number(cart[toDelete].quantity);
-        await updateProduct(product, productId);
-
-        const updated = cart.filter((p, i) => i !== toDelete);
-        updatedCart.products = updated;
-        setCart(updated);
-        await updateCart(updatedCart);
-    };
-
-    const increaseItemCount = async (e) => {
-        const updatedCart = await getCart();
-        const toChange = Number(e.target.parentElement.id);
-        const productId = cart[toChange].productId;
-        const product = getProduct(productId);
-        const currentQuantity = Number(cart[toChange].quantity) + 1;
-
-        if (currentQuantity > product.stock)
-            return alert(
-                `Only ${product.stock} available, please reduce the size of your order.`,
-            );
-
-        product.stock -= 1;
-        await updateProduct(product, productId);
-
-        updatedCart.products[toChange].quantity = currentQuantity;
-        setCart(updateCart.products);
-        await updateCart(updatedCart);
-    };
-
-    const decreaseItemCount = async (e) => {
-        const updatedCart = await getCart();
-        const toChange = Number(e.target.parentElement.id);
-        const productId = cart[toChange].productId;
-        const currentQuantity = Number(cart[toChange].quantity) - 1;
-        if (currentQuantity === 1) return e.target.disabled;
-
-        const product = getProduct(productId);
-        product.stock += 1;
-        await updateProduct(product, productId);
-
-        updatedCart.products[toChange].quantity = currentQuantity;
-        setCart(updateCart.products);
-        await updateCart(updatedCart);
-    };
-
     if (cart) {
         return (
             <div className={styles.Cart}>
                 <h1>Cart</h1>
                 {cart.map((p, i) => {
                     return (
-                        <div key={i} className={styles.Cart__Item} id={i}>
-                            <h2>{p.name}</h2>
-                            <p>{p.colour}</p>
-                            <button
-                                className={styles.Cart__Button}
-                                onClick={decreaseItemCount}
-                            >
-                                -
-                            </button>
-                            <p>{p.quantity}</p>
-                            <button
-                                className={styles.Cart__Button}
-                                onClick={increaseItemCount}
-                            >
-                                +
-                            </button>
-                            <p>
-                                item:{" "}
-                                {p.itemPrice.toLocaleString("en-AU", {
-                                    style: "currency",
-                                    currency: "AUD",
-                                    minimumFractionDigits: 2,
-                                })}
-                            </p>
-                            <p>
-                                total:{" "}
-                                {(p.itemPrice * p.quantity).toLocaleString(
-                                    "en-AU",
-                                    {
-                                        style: "currency",
-                                        currency: "AUD",
-                                        minimumFractionDigits: 2,
-                                    },
-                                )}
-                            </p>
-                            <button
-                                className={styles.Cart__Button}
-                                onClick={deleteItem}
-                            >
-                                x
-                            </button>
-                        </div>
+                        <CartItem
+                            cart={cart}
+                            setCart={setCart}
+                            products={products}
+                            product={p}
+                            key={i}
+                            i={i}
+                        />
                     );
                 })}
                 <div className={styles.Cart__Total}>
@@ -148,5 +53,4 @@ const Cart = () => {
         );
     }
 };
-
 export default Cart;
